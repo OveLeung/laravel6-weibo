14164891e9626806a63efc4408f4ce9dacf4451c
oveleung <254186540@qq.com>
2020-02-29 23:05:32 +0800

    发布微博

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Auth;
 use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
+    protected $fillable = ['content'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
